11add2bbc3bf52b74da4bb1cc1cd34dd5bbad2ce
Blake Nazario <blake.nazario@yahoo.com>
2016-06-19 16:27:59 -0400

    Added some documentation-formatting to HTTPClient.h

--- Base/HTTPClient.h
+++ Base/HTTPClient.h
@@ -10,19 +10,27 @@
 #import <UIKit/UIKit.h>
 
 typedef void (^CompleteGETRequest)(BOOL success, NSData *responseData, NSError *error);
+typedef void (^CompletePOSTRequest)(BOOL success, NSData *responseData, NSError *error);
 
 @interface HTTPClient : NSObject
 
 
 @property (copy, nonatomic) CompleteGETRequest completeGetRequest;
-
+@property (copy, nonatomic) CompletePOSTRequest completePostRequest;
 
 + (void)performGETrequestWithURL:(NSString*)urlString
-               completionBlock:(void (^)(BOOL, NSData *, NSError *))completionBlock;
+               completionBlock:(void (^)(BOOL, NSData *, NSError *))completionBlock; // completeGetRequest
 
 + (void)performPOSTrequestWithURL:(NSString*)urlString body:(NSString*)body
-                completionBlock:(void (^)(BOOL, NSData *, NSError *))completionBlock;
+                completionBlock:(void (^)(BOOL, NSData *, NSError *))completionBlock; // CompletePostRequest
 
+/**
+ Download an image from a specfied URL that directly links to an image.
+ 
+ @param url The URL that the image will be downloaded from in the form of an NSString.
+ 
+ @warning `url` must not be `nil`.
+ **/
 - (UIImage*)downloadImage:(NSString*)url;
 
 @end
